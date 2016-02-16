361d1f9f6caeabb5af04b69dfc7cf418bb91dbc4
luyongxi <gzluyongxi@gmail.com>
2016-02-16 10:57:00 -0800

    Update train_test_voc_07.sh

--- experiments/scripts/train_test_voc_07.sh
+++ experiments/scripts/train_test_voc_07.sh
@@ -14,14 +14,14 @@ time ./tools/train_az_net.py --gpu $1 \
   --weights data/imagenet_models/VGG16.v2.caffemodel \
   --imdb voc_2007_trainval \
   --cfg experiments/cfgs/vgg16_voc2007_train.yml \
-  --iters 80000
+  --iters 160000
 
 time ./tools/train_det_net.py --gpu $1 \
   --solver models/Pascal/VGG16/frcnn/solver.prototxt \
   --weights data/imagenet_models/VGG16.v2.caffemodel \
   --def models/Pascal/VGG16/az-net/test.prototxt \
   --def_fc models/Pascal/VGG16/az-net/test_fc.prototxt \
-  --net output/az-net/voc_2007_trainval/vgg16_az_net_iter_80000.caffemodel \
+  --net output/az-net/voc_2007_trainval/vgg16_az_net_iter_160000.caffemodel \
   --imdb voc_2007_trainval \
   --cfg experiments/cfgs/vgg16_voc2007_train.yml \
   --iters 80000
@@ -31,14 +31,14 @@ time ./tools/train_az_net.py --gpu $1 \
   --weights output/az-net/voc_2007_trainval/vgg16_fast_rcnn_iter_80000.caffemodel \
   --imdb voc_2007_trainval \
   --cfg experiments/cfgs/vgg16_voc2007_train.yml \
-  --iters 80000
+  --iters 160000
 
 time ./tools/train_det_net.py --gpu $1 \
   --solver models/Pascal/VGG16/frcnn/shared/solver.prototxt \
   --weights output/az-net/voc_2007_trainval/vgg16_fast_rcnn_iter_80000.caffemodel \
   --def models/Pascal/VGG16/az-net/test.prototxt \
   --def_fc models/Pascal/VGG16/az-net/test_fc.prototxt \
-  --net output/az-net/voc_2007_trainval/vgg16_az_net_shared_iter_80000.caffemodel \
+  --net output/az-net/voc_2007_trainval/vgg16_az_net_shared_iter_160000.caffemodel \
   --imdb voc_2007_trainval \
   --cfg experiments/cfgs/vgg16_voc2007_train.yml \
   --iters 80000
@@ -48,7 +48,7 @@ time ./tools/test_shared.py --gpu $1 \
   --net_frcnn output/az-net/voc_2007_trainval/vgg16_fast_rcnn_shared_iter_80000.caffemodel \
   --def_az models/Pascal/VGG16/az-net/test.prototxt \
   --def_fc_az models/Pascal/VGG16/az-net/test_fc.prototxt \
-  --net_az output/az-net/voc_2007_trainval/vgg16_az_net_shared_iter_80000.caffemodel \
+  --net_az output/az-net/voc_2007_trainval/vgg16_az_net_shared_iter_160000.caffemodel \
   --cfg experiments/cfgs/vgg16_voc2007.yml \
   --imdb voc_2007_test \
   --comp
