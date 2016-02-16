5f37ad56d38ac943120eb08ae16210d2b92d2dbe
luyongxi <gzluyongxi@gmail.com>
2016-02-16 10:57:58 -0800

    Update train_test_unshared_voc_07.sh

--- experiments/scripts/train_test_unshared_voc_07.sh
+++ experiments/scripts/train_test_unshared_voc_07.sh
@@ -14,14 +14,14 @@ time ./tools/train_az_net.py --gpu $1 \
   --weights data/imagenet_models/VGG16.v2.caffemodel \
   --imdb voc_2007_trainval \
   --cfg experiments/cfgs/vgg16_voc2007_train.yml \
-  --iters 80000
+  --iters 160000
 
 time ./tools/train_det_net.py --gpu $1 \
   --solver models/Pascal/VGG16/frcnn/solver.prototxt \
   --weights data/imagenet_models/VGG16.v2.caffemodel \
   --def models/Pascal/VGG16/az-net/test.prototxt \
   --def_fc models/Pascal/VGG16/az-net/test_fc.prototxt \
-  --net output/az-net/voc_2007_trainval/vgg16_az_net_iter_80000.caffemodel \
+  --net output/az-net/voc_2007_trainval/vgg16_az_net_iter_160000.caffemodel \
   --imdb voc_2007_trainval \
   --cfg experiments/cfgs/vgg16_voc2007_train.yml \
   --iters 80000
@@ -29,14 +29,14 @@ time ./tools/train_det_net.py --gpu $1 \
 time ./tools/prop_az.py --gpu $1 \
   --def models/Pascal/VGG16/az-net/test.prototxt \
   --def_fc models/Pascal/VGG16/az-net/test_fc.prototxt \
-  --net output/az-net/voc_2007_trainval/vgg16_az_net_iter_80000.caffemodel \
+  --net output/az-net/voc_2007_trainval/vgg16_az_net_iter_160000.caffemodel \
   --imdb voc_2007_test \
   --cfg experiments/cfgs/vgg16_voc2007.yml
 
 time ./tools/test_det_net.py --gpu $1 \
   --def models/Pascal/VGG16/frcnn/test.prototxt \
   --net output/az-net/voc_2007_trainval/vgg16_fast_rcnn_iter_80000.caffemodel \
-  --prop output/az-net/voc_2007_test/vgg16_az_net_iter_80000/proposals.pkl \
+  --prop output/az-net/voc_2007_test/vgg16_az_net_iter_160000/proposals.pkl \
   --imdb voc_2007_test \
   --cfg experiments/cfgs/vgg16_voc2007.yml \
   --comp
